ae2716ba4bc3b564026dab134c882104407d69e6
Sean Mooney <work@seanmooney.info>
2021-11-22 18:11:51 +0000

    Update templeated repo.
    This change replaces some of the boilerplate
    generated in the initial commit.
    
    This change removes the use of pytest for test authoring
    but keeps it for test running.
    
    This change updates tox and pre-commit we a basic set of
    default checks and test envionments.
    
    This change removes the use of black for linting, flake8
    and autopep8 will be used instead.
    
    This change adds a stub arbiterd-static console script

---
+++ src/arbiterd/cmd/static.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 - 2021, Sean Mooney and the arbiterd contributors
+# SPDX-License-Identifier: Apache-2.0
+
+def run():
+    print('Arbiterd ran successfully')
