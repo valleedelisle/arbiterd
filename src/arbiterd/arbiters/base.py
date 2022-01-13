f3b05eb19023434a96afbaa27cbdee51a852ee19
Sean Mooney <work@seanmooney.info>
2022-01-13 14:19:36 +0000

    add arbiter base class
    This change create a base arbiter class to define
    the arbiter interface
    
    this change also alters pre-commit to use reorder-python-imports
    instead of isort to allow it to work in a virtualenv

---
+++ src/arbiterd/arbiters/base.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 - 2021, Sean Mooney and the arbiterd contributors
+# SPDX-License-Identifier: Apache-2.0
+import abc
+from dataclasses import dataclass
+
+from arbiterd.objects import context as ctx
+
+
+@dataclass
+class ArbiterBase(abc.ABC):
+
+    TYPE: str = 'base'
+
+    @abc.abstractmethod
+    def arbitrate(self, context: ctx.Context) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def revoke(self, context: ctx.Context) -> str:
+        raise NotImplementedError
--- src/arbiterd/arbiters/cpu_state.py
+++ src/arbiterd/arbiters/cpu_state.py
@@ -1,29 +1,28 @@
 # -*- coding: utf-8 -*-
 # Copyright 2021 - 2021, Sean Mooney and the arbiterd contributors
 # SPDX-License-Identifier: Apache-2.0
-
 import logging
-from dataclasses import dataclass
 
-from arbiterd.common import cpu
+import typeing as ty
+from arbiterd.arbiters import base
+from arbiterd.objects import context as ctx
 from arbiterd.objects import hardware_thread
+
 
 LOG = logging.getLogger(__name__)
 
 
-@dataclass
-class CPUStateArbiter:
+class CPUStateArbiter(base.ArbiterBase):
     TYPE: str = 'cpu-state'
 
-    def arbitrate(self, context) -> str:
+    def arbitrate(self, context: ctx.Context) -> str:
         vm_cores = {
             core for instance in context.instances
             for field in instance.cpu_affinities for core in field}
-        managed_cores = {
-            hw_thread.ident for hw_thread in context.managed_hardware_threads
-        }
-        unused_cores = managed_cores - vm_cores
-        offlinable_cores = cpu.get_online_cpus() & managed_cores & unused_cores
+        offlinable_cores = (
+            self.get_online_cores(context.managed_hardware_threads) - vm_cores
+        )
+
         if context.dry_run:
             return f'offlinable_cpus: {offlinable_cores}'
 
@@ -34,13 +33,27 @@ class CPUStateArbiter:
 
         return f'offlined cpus: {offlinable_cores}'
 
-    def revoke(self, context) -> str:
+    def get_online_cores(
+            self, managed_cores: ty.List[hardware_thread.HardwareThread]
+    ) -> ty.Set[int]:
+        return {core.ident for core in managed_cores if core.online}
+
+    def get_offline_cores(
+            self, managed_cores: ty.List[hardware_thread.HardwareThread]
+    ) -> ty.Set[int]:
+        return {core.ident for core in managed_cores if not core.online}
+
+    def revoke(self, context: ctx.Context) -> str:
+        onlinable_cores = self.get_offline_cores(
+            context.managed_hardware_threads
+        )
+
         if context.dry_run:
-            return f'onlinable_cpus: {cpu.get_offline_cpus()}'
-        for core in cpu.get_offline_cpus():
+            return f'onlinable_cpus: {onlinable_cores}'
+        for core in onlinable_cores:
             logging.debug(f'onlining core: {core} as part of cpu_state revoke')
             hardware_thread.HardwareThread(ident=core).online = True
-        return 'onlined: {cpu.get_offline_cpus()}'
+        return 'onlined: {onlinable_cores}'
 
 
 def register(current_arbiters: dict) -> None:
